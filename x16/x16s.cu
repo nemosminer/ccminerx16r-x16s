6961b41bf70cdec5f87f8c3e186f65f1cda5afde
Brian Lee <brian112358@gmail.com>
2018-04-04 21:53:15 -0700

    Fixed issue with not submitting shares when hamsi is first algo.

--- x16/x16s.cu
+++ x16/x16s.cu
@@ -254,6 +254,7 @@ extern "C" int scanhash_x16s(int thr_id, struct work* work, uint32_t max_nonce, 
 		quark_keccak512_cpu_init(thr_id, throughput);
 		x11_shavite512_cpu_init(thr_id, throughput);
 		x11_simd512_cpu_init(thr_id, throughput); // 64
+		x13_hamsi512_cpu_init(thr_id, throughput);
 		x16_echo512_cuda_init(thr_id, throughput);
 		x16_fugue512_cpu_init(thr_id, throughput);
 		x15_whirlpool_cpu_init(thr_id, throughput, 0);
