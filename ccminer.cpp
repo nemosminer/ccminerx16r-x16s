6bc60bdfa5fc8b7045459d30bbe587d70243b27e
nemosminer <slbaker@hotmail.co.nz>
2018-05-19 18:48:53 +1200

    Update ccminer.cpp

--- ccminer.cpp
+++ ccminer.cpp
@@ -4507,8 +4507,9 @@ int main(int argc, char *argv[])
 	// get opt_quiet early
 	parse_single_opt('q', argc, argv);
 
-	printf("    \n\n");
-	printf("    THE1 Miner v1.0\n\n");
+	printf("*** ccminer " PACKAGE_VERSION " for nVidia GPUs by nemosminer@github ***\n");
+	printf("*** ccminer " PACKAGE_VERSION " compiled by nemosminer@github ***\n");
+
 	if (!opt_quiet) {
 		const char* arch = is_x64() ? "64-bits" : "32-bits";
 #ifdef _MSC_VER
@@ -4519,6 +4520,7 @@ int main(int argc, char *argv[])
 			CUDART_VERSION/1000, (CUDART_VERSION % 1000)/10, arch);
 		printf("  Originally based on Christian Buchner and Christian H. project\n");
 		printf("  Include some kernels from alexis78, djm34, djEzo, tsiv and krnlx.\n\n");
+		printf("			ccminer x16r-x16s-v0.6 \n\n");
 	}
 
 	rpc_user = strdup("");
